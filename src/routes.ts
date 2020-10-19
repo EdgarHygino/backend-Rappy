ff91d64c4578e4d6f82360a87c2380ebda6377f6
EdgarHygino <edgar.hygino@gmail.com>
2020-10-19 17:20:47 -0300

    dev-1: criado upload de imagens, add views, corrigindo detalhes do orfanato, add controle de exceçoes e validação de dados.

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from './config/upload';
 import OrphanagesController from "./controllers/OrphanagesController";
 
+
 const routes = Router();
+const upload = multer(uploadConfig);
+ 
 
 // metodos index, show, create, update, delete
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', OrphanagesController.create);
-
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create); 
 
 
 export default routes;
